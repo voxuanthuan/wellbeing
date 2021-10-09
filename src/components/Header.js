4497bbaa55e64b932374c8629a83ddf8a250f962
thuan vo <xuanthuan.vo@bnk.com>
2021-10-10 03:55:18 +0700

    responsive mobile first pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import logo from '../../public/BVS_logo.svg';
+import logoIcon from '../../public/logo-icon.svg';
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -6,8 +7,13 @@ export default function Header() {
     return (
         <div className="h-16 flex items-end pl-16 bg-transparent">
             <Link href="/" passHref>
-                <div className="w-[428px] hover:cursor-pointer">
-                    <Image src={logo} alt="BVS Logo" />
+                <div>
+                    <div className="hidden md:block w-[428px] hover:cursor-pointer">
+                        <Image src={logo} alt="BVS Logo" />
+                    </div>
+                    <div className="block md:hidden w-[428px] hover:cursor-pointer">
+                        <Image src={logoIcon} alt="BVS Logo" />
+                    </div>
                 </div>
             </Link>
         </div>
--- src/pages/index.js
+++ src/pages/index.js
@@ -29,9 +29,9 @@ export default function Index() {
       <Head>
         <title>BVIS WELLBEING</title>
       </Head>
-      <div className="w-full h-heightBody">
-        <div className="pl-0 lg:pl-16 xl:pl-48 h-full flex flex-col justify-around">
-          <div className="font-bold text-4xl lg:text-4xl xl:text-5xl 2xl:text-7xl pt-10">
+      <div className="w-full h-full md:h-heightBody">
+        <div className="pl-0 md:pl-6 lg:pl-16 xl:pl-48 h-full flex flex-col justify-around">
+          <div className="font-bold text-2xl md:text-4xl lg:text-4xl xl:text-5xl 2xl:text-7xl pt-0 md:pt-10">
             {
               i18n.language !== "vi-VN" ? (
                 <div>
@@ -49,7 +49,7 @@ export default function Index() {
               )
             }
           </div>
-          <div className="flex flex-col lg:flex-row h-heightMinusWellBeing pt-11 lg:pt-0 pb-0 xl:pb-11">
+          <div className="flex flex-col lg:flex-row h-heightMinusWellBeing pt-0 md:pt-11 lg:pt-0 pb-0 xl:pb-11">
               <div className=" lg:w-[360px] 2xl:w-[480px]">
                 <div className="h-full flex flex-col lg:flex-row items-start lg:items-end justify-between">
                   <div className="space-x-4">
@@ -70,9 +70,11 @@ export default function Index() {
                 </div>
               </div>
               <div className="flex-1">
-                <div className="w-full h-full flex items-end">
+                <div className="w-full h-full flex items-end md:justify-center justify-start">
                   <div className="relative z-10">
-                    <div className={"absolute top-[110px] sm:top-[120px] md:top-[130px] left-5 2xl:left-6 z-20 animate-hide-and-up-2 text-[12px] sm:text-[14px] lg:text-[15px] " + (i18n.language === "vi-VN" ? " lg:top-[90px] lg:left-4 lg:text-[12px] xl:text-[13px] 2xl:text-[16px] 2xl:top-32 xl:top-[98px] xl:left-5" : " lg:top-[90px] lg:left-5 lg:text-[14px] xl:text-[14px] 2xl:text-[19px] 2xl:top-32 xl:top-[98px] xl:left-5 2xl:left-7")}>
+                    <div className={"absolute top-[110px] sm:top-[120px] md:left-7 lg:left-5 2xl:left-6 z-20 animate-hide-and-up-2 text-[12px] sm:text-[14px] md:text-base lg:text-[15px] " 
+                    + (i18n.language === "vi-VN" ? "md:top-[108px]  lg:top-[90px] md:left-[21px] lg:left-4 text-[10px] md:text-[14px] lg:text-[12px] xl:text-[13px] 2xl:text-[16px] 2xl:top-32 xl:top-[98px] xl:left-5" 
+                    : " top-[50px] md:top-[110px]  lg:top-[90px] left-3 md:left-5 text-[7px] lg:text-[14px] xl:text-[14px] 2xl:text-[19px] 2xl:top-32 xl:top-[98px] xl:left-5 2xl:left-7")}>
                       <p className="font-medium"dangerouslySetInnerHTML={{__html: t('hi')}}/>
                       <input ref={inputEl} value={name} onChange={(e) => setName(e.target.value)}
                         className={"focus:placeholder-opacity-40 bg-transparent text-bvsPeach font-medium focus:outline-none placeholder-[#FF3750] " + (i18n.language === "vi-VN" ? "sm:w-[100px] lg:w-[100px] 2xl:w-[126px]": "sm:w-[100px] lg:w-[80px] xl:w-[100px] 2xl:w-[120px]")} placeholder={i18n.language === 'vi-VN' ? 'Tên bạn' : 'Your Name'}
@@ -80,25 +82,27 @@ export default function Index() {
                       <p className="font-medium">{t('amHappie')}</p>
                     </div>
                     <div className={"absolute font-semibold animate-hide-and-up-3 w-auto lg:w-[310px] "
-                      + (i18n.language === "vi-VN" ? " -right-16 top-3 md:-right-6 lg:-right-20 lg:top-3 xl:-right-20 xl:top-2 2xl:right-0 2xl:top-3 z-20 text-[19px] xl:text-[21px] 2xl:text-[27px]": " -right-16 top-3 md:-right-6 lg:-right-20 lg:top-4 xl:-right-16 xl:top-2 2xl:right-0 2xl:top-5 z-20 text-[22px] xl:text-[28px] 2xl:text-[32px]" )
+                      + (i18n.language === "vi-VN" ?
+                       " -right-16 top-3 md:right-12 lg:-right-20 lg:top-3 xl:-right-20 xl:top-2 2xl:right-0 2xl:top-3 z-20 text-[23px] lg:text-[19px] xl:text-[21px] 2xl:text-[27px]":
+                       " right-7 top-1 md:right-16 lg:-right-20 lg:top-4 xl:-right-16 xl:top-2 2xl:right-0 2xl:top-5 z-20 text-[14px] md:text-[30px] lg:text-[22px] xl:text-[28px] 2xl:text-[32px]" )
                     }>
                       <p dangerouslySetInnerHTML={{__html: t('weAre')}} />
                     </div>
-                    <div className="w-[330px] lg:w-[550px] xl:w-[600px] 2xl:w-[766px]">
+                    <div className="w-[300px] sm:w-[400px] md:w-[650px] lg:w-[550px] xl:w-[600px] 2xl:w-[766px]">
                       <Image alt="title mascot" src={titleMascot}/>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="lg:hidden mt-4 mx-auto pr-[46px]">
-                      <button onClick={handleOnClick} className="hover:scale-105 w-[130px] h-9 border-1 rounded-[17px] bg-bvsPeach text-white font-medium text-base flex items-center justify-center">
+              <div className="lg:hidden mt-4 mb-2 mx-auto">
+                      <button onClick={handleOnClick} className="hover:scale-105 w-[146px] h-[42px] lg:w-[130px] lg:h-9 border-1 rounded-[17px] bg-bvsPeach text-white font-medium text-base flex items-center justify-center">
                           <a>{t('next').toLocaleUpperCase()}</a>
                           <div className="ml-3">
                             <Image alt="next arrow" src={NextArrow}/>
                           </div>
                     </button>
                   </div>
-            </div>
+              </div>
         </div>
 
       </div>
