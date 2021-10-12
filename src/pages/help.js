008a5a440dc761ef2a29007ea6c2c5ea00d46767
thuan vo <xuanthuan.vo@bnk.com>
2021-10-12 07:04:42 +0700

    fix responsive for page help

--- src/pages/help.js
+++ src/pages/help.js
@@ -19,17 +19,17 @@ export default function Connect() {
     const {name} = useAppContext();
 
     return (
-        <div className="bg-bvsTealLight">
+        <div className="bg-bvsTealLight h-full">
             <div className="relative">
                 <Header />
-                <div className="h-heightBody w-full flex justify-center items-center">
+                <div className="h-full lg:h-heightBody w-full flex justify-center items-center">
                    <div className="w-full">
                     <div className="w-full mx-auto  text-center">
-                        <p className="font-semibold text-[24px] mx-auto w-[740px] text-bvsNavy pt-5">{t('helpTitle')}</p>
+                        <p className="font-semibold text-[18px] sm:text-[24px] mx-auto w-auto lg:w-[740px] text-bvsNavy pt-5">{t('helpTitle')}</p>
                         </div>
-                        <div className="flex justify-around mt-10 2xl:mt-20 w-[900px] mx-auto space-x-12">
-                            <div className="flex flex-col items-center max-w-[204px]">
-                                <div className="hover:scale-110 w-[151px] relative">
+                        <div className="flex flex-col items-center justify-center lg:flex-row lg:justify-around mt-10 2xl:mt-20 w-full lg:w-[900px] mx-auto space-x-0 lg:space-x-12">
+                            <div className="flex flex-row lg:flex-col items-center sm:w-[400px] md:min-w-[500px] lg:min-w-0 max-w-[500px] lg:max-w-[204px] mb-6 lg:mb-0">
+                                <div className="hover:scale-110 w-[115px] sm:w-[151px] relative">
                                     <div className="w-full h-full">
                                         <Image alt="frame image" src={porTraitFrame}/>
                                     </div>
@@ -37,10 +37,10 @@ export default function Connect() {
                                         <Image alt="teacher image" src={teacherImage}/>
                                     </div>
                                 </div>
-                                <p className="font-semibold text-base text-bvsDrakTeal mt-[21px] text-center">{t('formTutor')}</p>
+                                <p className="font-semibold text-base text-bvsDrakTeal mt-[21px] text-center ml-1 sm:ml-5 lg:ml-0">{t('formTutor')}</p>
                             </div>
-                            <div className="flex flex-col items-center max-w-[204px] relative">
-                                <div className="hover:scale-110 w-[151px] relative">
+                            <div className="flex flex-row lg:flex-col items-center sm:w-[400px] md:min-w-[500px] lg:min-w-0 max-w-[500px] lg:max-w-[204px] mb-6 lg:mb-0 relative">
+                                <div className="hover:scale-110 w-[115px] sm:w-[151px] relative">
                                     <div className="w-full h-full">
                                         <Image alt="frame image" src={porTraitFrame}/>
                                     </div>
@@ -48,32 +48,29 @@ export default function Connect() {
                                         <Image alt="teacher image" src={headImage}/>
                                     </div>
                                 </div>
-                                <p className="font-semibold text-base text-bvsDrakTeal mt-[21px] text-center">{t('headOfHouse')}</p>
+                                <p className="font-semibold text-base text-bvsDrakTeal mt-[21px] text-center ml-1 sm:ml-5 lg:ml-0 max-w-[170px]">{t('headOfHouse')}</p>
                             </div>
-                            <div className="flex flex-col items-center max-w-[204px]">
-                                <div className="hover:scale-110 w-[151px] relative overflow-hidden">
-                                    <div className="w-full h-full">
+                            <div className="flex flex-row lg:flex-col items-center sm:w-[400px] md:min-w-[500px] lg:min-w-0 max-w-[500px] lg:max-w-[204px] mb-6 lg:mb-0">
+                                <div className="hover:scale-110  relative">
+                                    <div className="w-[115px] sm:w-[151px]">
                                         <Image alt="Mr.Luke Curran image" src={mrLukeCurranImage}/>
                                     </div>
-                                    {/* <div className="absolute z-10 top-[7px] right-[19px]">
-                                        <Image alt="Mr Luke Curran" src={mrLukeCurranImage}/>
-                                    </div> */}
                                 </div>
-                                <span className="font-semibold text-base text-bvsDrakTeal mt-[21px] text-center" dangerouslySetInnerHTML={{__html: t('mrLukeCurran')}} />
-                                <span className="font-semibold text-sm text-bvsDrakTeal text-center">Luke.Curran@bvisivietnam.com 
-                                </span>
+                                <div className="ml-1 sm:ml-5 lg:ml-0 text-center max-w-[190px]">
+                                    <span className="font-semibold text-base text-bvsDrakTeal mt-[21px] text-center" dangerouslySetInnerHTML={{__html: t('mrLukeCurran')}} />
+                                    <span className="font-semibold text-[11px] sm:text-sm text-bvsDrakTeal text-center">Luke.Curran@bvisivietnam.com </span>
+                                </div>
                             </div>
-                            <div className="flex flex-col items-center max-w-[204px]">
-                                <div className="hover:scale-110 w-[151px] relative">
-                                    <div className="w-full h-full">
+                            <div className="flex flex-row lg:flex-col items-center sm:w-[400px] md:min-w-[500px] lg:min-w-0 max-w-[500px] lg:max-w-[204px] mb-6 lg:mb-0">
+                                <div className="hover:scale-110 relative">
+                                    <div className=" w-[115px] sm:w-[151px]">
                                         <Image alt="frame image" src={mailBoxImage}/>
                                     </div>
-                                    {/* <div className="absolute z-10 top-[58px] right-[27px]">
-                                        <Image alt="Mr Luke Curran" src={mailBoxImage}/>
-                                    </div> */}
                                 </div>
-                                <p className="font-semibold text-base text-bvsDrakTeal mt-[21px] text-center">{t('worryBox')}</p>
-                                <p className="font-semibold text-sm text-bvsDrakTeal text-center">Theworrybox@bvisivietnam.com​</p>
+                                <div className="ml-1 sm:ml-5 lg:ml-0 max-w-[190px] lg:max-w-[100%] text-center">
+                                    <p className="font-semibold text-base text-bvsDrakTeal mt-[21px]">{t('worryBox')}</p>
+                                    <p className="font-semibold text-[11px] sm:text-sm text-bvsDrakTeal">Theworrybox@bvisivietnam.com​</p>
+                                </div>
                             </div>
                         </div>
                         <p className="font-semibold text-[36px] text-center text-bvsPeach mt-[30px] 2xl:mt-[40px]">{`${name}, ${t('toHelp').toLocaleUpperCase()}`}</p>
